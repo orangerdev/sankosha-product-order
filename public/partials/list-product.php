f7fb3eb89c0e2886de258ab90eb4540df7a82a02
Ridwan Arifandi <hi@ridwanarifandi.com>
2019-07-05 10:44:34 +0700

    display price in front end

--- public/partials/list-product.php
+++ public/partials/list-product.php
@@ -20,6 +20,11 @@
                 <?php the_title(); ?>
             </a>
         </h2>
+        <?php if(is_user_logged_in()) : ?>
+        <div class="price">
+            <?php echo carbon_get_the_post_meta('price'); ?>
+        </div>
+        <?php endif; ?>
         <a href="<?php the_permalink(); ?>" title='<?php the_title_attribute(); ?>' class='button'>
             <?php _e('Pesan','snkpo'); ?>
         </a>
